32225baa42cbded8ead2cdebd1ddfadc8c8f4c8b
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2023-06-20 19:58:31 +0530

    signin form complete

--- components/login.jsx
+++ components/login.jsx
@@ -6,28 +6,25 @@ const Login = () => {
     const onSubmit = data => console.log(data);
   
   return (
-   <>
-   <section>
-   <div className='relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100'>
-
-   <div className='relative sm:max-w-sm w-full'>
-    <div className='bg-blue-400 shadow-lg w-full h-full rounded-3xl absolute transform -rotate-6'>
-        <div className='bg-red-400 shadow-lg w-full h-full rounded-3xl absolute transform rotate-6'>
-        <div className='relative w-full rounded-3xl ox-6 py-4 bg-gray-100 shadow-md'>
-            <label for="" className='block mt-3 text-sm text-gray-700 text-center font-semibold '>
-                
-            </label>
-            
-        </div>
-
-        </div>
+<>
+ 
+ <div class="h-screen bg-indigo-200 flex justify-center items-center">
+  <form class="w-full max-w-xs bg-white flex flex-col py-5 px-8 rounded-lg shadow-lg" action="">
+    <label class="text-gray-700 font-bold py-2" for="">Username</label>
+    <input class="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3" type="text" placeholder="Username" />
+    <label class="text-gray-700 font-bold py-2" for="">Password</label>
+    <input class="text-gray-700 shadow border rounded border-gray-300 mb-3 py-1 px-3 focus:outline-none focus:shadow-outline" type="password" placeholder="********" />
+    <div class="flex justify-between items-center my-4">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4">
+        Sign In
+      </button>
+      <a class="text-blue-600 hover:text-blue-800 font-bold" href="#">
+        Forgot Password?
+      </a>
     </div>
-   </div>
-
-
-
-   </div>
-   </section>
+    
+  </form>
+</div>
 
 
    </>
